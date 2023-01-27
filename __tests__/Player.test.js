09837c24a5764fc11fb0f71dd61279c767afe8a4
edm1001 <edmer_franciz@yahoo.com>
2023-01-27 14:28:38 -0600

    add health properties to Player

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -38,3 +38,31 @@ test('creates player object', () => {
 
         expect(player.getInventory()).toEqual(false);
     });
+    
+    test('gets player health and value', () => {
+        const player = new Player('Dave');
+
+        expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+    })
+
+    // checks if player is alive or not
+    test('checks if player is alive', () => {
+        const player = new Player('Dave');
+
+        expect(player.isAlive()).toBeTruthy();
+        player.health = 0;
+        expect(player.isAlive()).toBeFalsy();
+
+    })
+
+    test('subtracts from player health', () => {
+        const player = new Player('Dave');
+        const oldHealth = player.health;
+
+        player.reduceHealth(5);
+
+        expect(player.health).toBe(oldHealth - 5);
+
+        player.reduceHealth(99999);
+        expect(player.health).toBe(0);
+    })
