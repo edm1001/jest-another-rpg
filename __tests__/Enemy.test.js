455f62672169b082eee84e1859e94d8816ed92cf
edm1001 <edmer_franciz@yahoo.com>
2023-02-10 22:58:42 -0600

    add edit to enemy test

--- __tests__/Enemy.test.js
+++ __tests__/Enemy.test.js
@@ -12,5 +12,28 @@ test('creates an enemy object', () => {
     expect(enemy.strength).toEqual(expect.any(Number));
     expect(enemy.agility).toEqual(expect.any(Number));
     expect(enemy.potion).toEqual(expect.any(Object));
+})
 
+test('gets enemy health value', () => {
+    const enemy = new Enemy('goblin', 'sword');
+
+    expect(enemy.getHealth()).toEqual(expect.stringContaining(enemy.health.toString()));
+})
+
+test('checks if enemy is alive or not', ()=> {
+    const enemy = new Enemy('goblin', 'sword');
+
+    expect(enemy.isAlive()).toBeTruthy();
+
+    enemy.health = 0 ;
+
+    expect(enemy.isAlive()).toBeFalsy();
+})
+
+test("gets enemy's attack value", () => {
+    const enemy = new Enemy('goblin', 'sword');
+    enemy.strength = 10;
+
+    expect(enemy.getAttackValue()).toBeGreaterThanOrEqual(5);
+    expect(enemy.getAttackValue()).toBeLessThanOrEqual(15)
 })
